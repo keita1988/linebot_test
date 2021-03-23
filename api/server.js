6961773fde9db31d184a331a33cf290fad2bae27
keita1988 <71539570+keita1988@users.noreply.github.com>
2021-03-23 14:52:00 +0900

    webhook

--- api/server.js
+++ api/server.js
@@ -44,3 +44,5 @@ async function handleEvent(event) {
 // console.log(`Server running at ${PORT}`);
 (process.env.NOW_REGION) ? module.exports = app : app.listen(PORT);
 console.log(`Server running at ${PORT}`);
+
+
